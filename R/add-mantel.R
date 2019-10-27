a8aeab146df899186525244a698f70108f76efe9
houyun <houyunhuang@163.com>
2019-10-27 23:58:18 +0800

    add S3 method

--- R/add-mantel.R
+++ R/add-mantel.R
@@ -1,56 +1,18 @@
 #' @export
-add_mantel <- function(mantel,
-                       on.left = FALSE,
-                       diag.label = FALSE,
-                       r.breaks = c(0.25, 0.5),
-                       r.labels = c("<= 0.25", "0.25 - 0.5", "> 0.5"),
-                       p.breaks = c(0.001, 0.01, 0.05),
-                       p.labels = c("<= 0.001", "0.001 - 0.01", "0.01 - 0.05", "> 0.05"),
-                       link.line.colours = NULL,
-                       link.line.size = c(0.5, 1.5, 4),
-                       legend.title.scale.size = "Mantel's r",
-                       legend.title.scale.colour = "p value",
-                       legend.drop = FALSE,
-                       ...)
-{
-  if(!inherits(mantel, "mantel_tbl"))
-    stop("'mantel' need a mantel_tbl.", call. = FALSE)
-  mantel <- rename_mantel(mantel)
-  mapping <- aes_string(size = "r", colour = "p")
-  r.breaks <- unique(sort(c(-1, r.breaks, 1)))
-  p.breaks <- unique(sort(c(0, p.breaks, 1)))
-  mantel$r <- cut(mantel$r, breaks = r.breaks, labels = r.labels,
-                     include.lowest = FALSE)
-  mantel$p <- cut(mantel$p, breaks = p.breaks, labels = p.labels,
-                     include.lowest = FALSE)
-  if(is.null(link.line.colours))
-    link.line.colours <- link_colour_pal(length(p.breaks) - 1)
-  scale <- list(link.line.colour = scale_colour_manual(drop = legend.drop, values = link.line.colours),
-                link.line.size   = scale_size_manual(drop = legend.drop, values = link.line.size),
-                link.line.guide  = guides(
-                  colour = guide_legend(title = legend.title.scale.colour,
-                                        override.aes = list(size = 2), order = 2),
-                  size = guide_legend(title = legend.title.scale.size,
-                                      override.aes = list(colour = "grey35"), order = 1)))
-  link <- add_link(df = mantel,
-                   mapping = mapping,
-                   on.left = on.left,
-                   diag.label = diag.label,
-                   ...)
-  list(link, scale)
+add_link <- function(x, ...) {
+  UseMethod("add_link")
 }
 
-
 #' @export
-add_link <- function(df,
-                     mapping = NULL,
-                     on.left = FALSE,
-                     diag.label = FALSE,
-                     curvature = NULL,
-                     group.label.hspace = NULL,
-                     group.label.vspace = 0,
-                     extra.params = extra_params(),
-                     ...)
+add_link.data.frame <- function(x,
+                             mapping = NULL,
+                             on.left = FALSE,
+                             diag.label = FALSE,
+                             curvature = NULL,
+                             group.label.hspace = NULL,
+                             group.label.vspace = 0,
+                             extra.params = extra_params(),
+                             ...)
 {
   link_fun <- function(corr) {
     if(!inherits(corr, "cor_tbl"))
@@ -61,7 +23,7 @@ add_link <- function(df,
     m <- length(cor_tbl_yname(corr))
     if(type != "full")
       on.left <- FALSE
-    data <- handle_link_data(df, corr, on.left = on.left, diag.label = diag.label,
+    data <- handle_link_data(x = x, corr, on.left = on.left, diag.label = diag.label,
                              corrmat.point.hjust = extra.params$link.params$corrmat.point.hjust,
                              corrmat.point.vjust = extra.params$link.params$corrmat.point.vjust,
                              group.point.hjust = extra.params$link.params$group.point.hjust,
@@ -73,7 +35,7 @@ add_link <- function(df,
       mapping <- aes_string(x = "group_x", y = "group_y", xend = "corr_x", yend = "corr_y")
     } else {
       mapping <- modifyList(aes_string(x = "group_x", y = "group_y", xend = "corr_x", yend = "corr_y"),
-                                       mapping)
+                            mapping)
     }
 
     if(is.null(curvature)) {
@@ -84,9 +46,9 @@ add_link <- function(df,
     }
     if(is.null(group.label.hspace)) {
       group.label.hspace <- switch (type,
-                                   full = if(on.left) - 0.2 else 0.2,
-                                   upper = -0.2,
-                                   lower = 0.2)
+                                    full = if(on.left) - 0.2 else 0.2,
+                                    upper = -0.2,
+                                    lower = 0.2)
     }
     if(is.null(extra.params$group.label$hjust)) {
       extra.params$group.label$hjust <- switch(
@@ -139,6 +101,92 @@ add_link <- function(df,
   }
   class(link_fun) <- c("link_fun", class(link_fun))
   link_fun
+}
+
+#' @export
+add_link.mantel_tbl <- function(x,
+                       on.left = FALSE,
+                       diag.label = FALSE,
+                       r.breaks = c(0.25, 0.5),
+                       r.labels = c("<= 0.25", "0.25 - 0.5", "> 0.5"),
+                       p.breaks = c(0.001, 0.01, 0.05),
+                       p.labels = c("<= 0.001", "0.001 - 0.01", "0.01 - 0.05", "> 0.05"),
+                       link.line.colours = NULL,
+                       link.line.size = c(0.5, 1.5, 4),
+                       legend.title.scale.size = "Mantel's r",
+                       legend.title.scale.colour = "p value",
+                       legend.drop = FALSE,
+                       ...)
+{
+  mantel <- rename_mantel(x)
+  mapping <- aes_string(size = "r", colour = "p")
+  r.breaks <- unique(sort(c(-1, r.breaks, 1)))
+  p.breaks <- unique(sort(c(0, p.breaks, 1)))
+  mantel$r <- cut(mantel$r, breaks = r.breaks, labels = r.labels,
+                     include.lowest = FALSE)
+  mantel$p <- cut(mantel$p, breaks = p.breaks, labels = p.labels,
+                     include.lowest = FALSE)
+  if(is.null(link.line.colours))
+    link.line.colours <- link_colour_pal(length(p.breaks) - 1)
+  scale <- list(link.line.colour = scale_colour_manual(drop = legend.drop, values = link.line.colours),
+                link.line.size   = scale_size_manual(drop = legend.drop, values = link.line.size),
+                link.line.guide  = guides(
+                  colour = guide_legend(title = legend.title.scale.colour,
+                                        override.aes = list(size = 2), order = 2),
+                  size = guide_legend(title = legend.title.scale.size,
+                                      override.aes = list(colour = "grey35"), order = 1)))
+  link <- add_link.data.frame(x = mantel,
+                   mapping = mapping,
+                   on.left = on.left,
+                   diag.label = diag.label,
+                   ...)
+  list(link, scale)
+}
+
+
+#' @export
+add_link.cor_tbl <- function(x,
+                             on.left = FALSE,
+                             diag.label = FALSE,
+                             r.breaks = c(0.25, 0.5),
+                             r.labels = c("<= 0.25", "0.25 - 0.5", "> 0.5"),
+                             p.breaks = c(0.001, 0.01, 0.05),
+                             p.labels = c("<= 0.001", "0.001 - 0.01", "0.01 - 0.05", "> 0.05"),
+                             link.line.colours = NULL,
+                             link.line.size = c(0.5, 1.5, 4),
+                             legend.title.scale.size = "correlation",
+                             legend.title.scale.colour = "p value",
+                             legend.drop = FALSE,
+                             ...)
+{
+  cor_tbl <- cor_tbl_namebind(x)
+  mapping <- aes_string(size = "r", colour = "p")
+  r.breaks <- unique(sort(c(-1, r.breaks, 1)))
+  p.breaks <- unique(sort(c(0, p.breaks, 1)))
+  cor_tbl$r <- cut(cor_tbl$r, breaks = r.breaks, labels = r.labels,
+                  include.lowest = FALSE)
+  cor_tbl$p <- cut(cor_tbl$p, breaks = p.breaks, labels = p.labels,
+                  include.lowest = FALSE)
+  if(is.null(link.line.colours))
+    link.line.colours <- link_colour_pal(length(p.breaks) - 1)
+  scale <- list(link.line.colour = scale_colour_manual(drop = legend.drop, values = link.line.colours),
+                link.line.size   = scale_size_manual(drop = legend.drop, values = link.line.size),
+                link.line.guide  = guides(
+                  colour = guide_legend(title = legend.title.scale.colour,
+                                        override.aes = list(size = 2), order = 2),
+                  size = guide_legend(title = legend.title.scale.size,
+                                      override.aes = list(colour = "grey35"), order = 1)))
+  link <- add_link.data.frame(x = cor_tbl,
+                              mapping = mapping,
+                              on.left = on.left,
+                              diag.label = diag.label,
+                              ...)
+  list(link, scale)
+}
+
+#' @export
+add_link.default <- function(x, ...) {
+  stop(class(x), " hasn't been realized yet.", call. = FALSE)
 }
 
 #' @export
