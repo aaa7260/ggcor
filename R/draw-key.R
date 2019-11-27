1d21fa7464fbfbe65cc804a1d77046bc42809175
houyun <houyunhuang@163.com>
2019-11-27 22:27:08 +0800

    add draw_key_ellipse

--- R/draw-key.R
+++ R/draw-key.R
@@ -1,4 +1,27 @@
 #' @export
+draw_key_ellipse <- function(data, params, size) {
+  ellipseGrob(0.5, 0.5, data$r %||% 0,
+              gp = gpar(
+                col = scales::alpha(data$colour %||% NA),
+                fill = scales::alpha(data$fill %||% "grey20", data$alpha),
+                lty = data$linetype %||% 1,
+                lwd = (data$size %||% 0.5) * .pt
+              ))
+}
+#' @noRd
+ellipseGrob <- function(x = 0.5, y = 0.5, r = 1, n = 100,
+                        gp = gpar(colour = "grey50", fill = "grey90"),
+                        default.units = "native", ...) {
+  no_na_r <- r[!is.na(r)]
+  if(any(no_na_r > 1 | no_na_r < -1)) {
+    warning("'r' needs to be in the range -1 to 1.", call. = FALSE)
+    r <- ifelse(r > 1 | r < -1, NA, r)
+  }
+  dd <- point_to_ellipse(x, y, r, n)
+  grid::polygonGrob(dd$x, dd$y, default.units = default.units, gp = gp, ...)
+}
+
+#' @export
 draw_key_square <- function(data, params, size) {
   grid::rectGrob(0.5, 0.5,
                  width = 2 * abs(data$r),
