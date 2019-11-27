d97c60b8d5ac85b2c47fccc3b75ec82baece629b
houyun <houyunhuang@163.com>
2019-11-27 22:10:02 +0800

    add draw_key_pie

--- R/draw-key.R
+++ R/draw-key.R
@@ -1,4 +1,19 @@
 #' @export
+draw_key_pie <- function(data, params, size){
+  pieGrob(0.5, 0.5, data$r %||% 0, params$n %||% 100)
+}
+#' @noRd
+pieGrob <- function(x = 0.5, y = 0.5, r = 0.5, n = 100,
+                    gp = gpar(colour = "grey50", fill = "grey90"),
+                    default.units = "npc", ...) {
+  line <- point_to_line(x, y, r, n)
+  sector <- point_to_sector(x, y, r, n)
+  grid::grobTree(
+    grid::linesGrob(line$x, line$y, default.units = default.units, gp = gp, ...),
+    grid::polygonGrob(sector$x, sector$y, default.units = default.units, gp = gp, ...)
+  )
+}
+#' @export
 draw_key_cross <- function(data, params, size) {
   grid::segmentsGrob(c(0.15, 0.15), c(0.15, 0.85), c(0.85, 0.85), c(0.85, 0.15),
                      gp = grid::gpar(
