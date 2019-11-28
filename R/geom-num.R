3f811b883786367df6ec8490a7e73f1e7aee17ce
houyun <houyunhuang@163.com>
2019-11-28 20:53:36 +0800

    update geom_number()

--- R/geom-num.R
+++ R/geom-num.R
@@ -1,4 +1,4 @@
-#' Formats texts based on center.
+#' Format number Geom
 #'
 #'
 #' @eval rd_aesthetics("geom", "num")
@@ -18,11 +18,13 @@
 #' @importFrom ggplot2 aes
 #' @importFrom ggplot2 GeomText
 #' @importFrom ggplot2 draw_key_text
-geom_num <- function(mapping = NULL, data = NULL,
+geom_number <- function(mapping = NULL, data = NULL,
                       stat = "identity", position = "identity",
                       ...,
                       nudge_x = 0,
                       nudge_y = 0,
+                      digits = 2,
+                      nsmall = 2,
                       na.rm = FALSE,
                       show.legend = NA,
                       inherit.aes = TRUE)
@@ -37,11 +39,13 @@ geom_num <- function(mapping = NULL, data = NULL,
     data = data,
     mapping = mapping,
     stat = stat,
-    geom = GeomNum,
+    geom = GeomNumber,
     position = position,
     show.legend = show.legend,
     inherit.aes = inherit.aes,
     params = list(
+      digits = digits,
+      nsmall = nsmall,
       na.rm = na.rm,
       ...
     )
@@ -52,9 +56,9 @@ geom_num <- function(mapping = NULL, data = NULL,
 #' @format NULL
 #' @usage NULL
 #' @export
-GeomNum <- ggproto("GeomNum",
+GeomNumber <- ggproto("GeomNumber",
                    GeomText,
-                   required_aes = c("x", "y", "num"),
+                   required_aes = c("x", "y", "number"),
 
                    default_aes = aes(
                      colour = "black", size = 3.88, angle = 0, hjust = 0.5,
@@ -62,7 +66,7 @@ GeomNum <- ggproto("GeomNum",
 
                    draw_panel = function(data, panel_params, coord, digits = 2, nsmall = 2,
                                          na.rm = FALSE) {
-                     data$label <- format_number(data$num, digits, nsmall)
+                     data$label <- format_number(data$number, digits, nsmall)
                      GeomText$draw_panel(data, panel_params, coord)
                     },
 
