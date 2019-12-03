a50dd73792b01d797042aafe10ce3fd209e219f2
houyun <houyunhuang@163.com>
2019-12-03 21:19:20 +0800

    updat geom_cross()

--- R/geom-cross.R
+++ R/geom-cross.R
@@ -2,28 +2,24 @@
 #'
 #'
 #' @eval rd_aesthetics("geom", "cross")
-#' @param r0 the radius of outer circle, defualt value is 0.6.
 #' @param conf.level confidence level.
-#' @param labels labels for the levels of different confidence levels.
 #'
 #' @inheritParams ggplot2::layer
-#' @inheritParams ggplot2::geom_segment
+#' @inheritParams ggplot2::geom_point
 #' @rdname geom_cross
 #' @export
 #' @importFrom ggplot2 layer
 #' @importFrom ggplot2 ggproto
 #' @importFrom ggplot2 aes
 #' @importFrom ggplot2 Geom
-#' @importFrom ggplot2 GeomSegment
-#' @importFrom ggplot2 draw_key_blank
-#' @importFrom grid grobTree
+#' @importFrom ggplot2 GeomPoint
+#' @importFrom ggplot2 draw_key_point
 
 
 geom_cross <- function(mapping = NULL, data = NULL,
                        stat = "identity", position = "identity",
                        ...,
                        conf.level = 0.05,
-                       r0 = 0.6,
                        na.rm = FALSE,
                        show.legend = NA,
                        inherit.aes = TRUE) {
@@ -37,7 +33,6 @@ geom_cross <- function(mapping = NULL, data = NULL,
     inherit.aes = inherit.aes,
     params = list(
       conf.level = conf.level,
-      r0 = r0,
       na.rm = na.rm,
       ...
     )
@@ -49,33 +44,15 @@ geom_cross <- function(mapping = NULL, data = NULL,
 #' @usage NULL
 #' @export
 GeomCross <- ggproto(
-  "GeomCross", Geom,
-  default_aes = aes(colour = "red", fill = NA,
-                    size = 0.5, linetype = 1, alpha = NA),
+  "GeomCross", GeomPoint,
+  default_aes = aes(shape = 4, colour = "red", size = 5, fill = NA,
+                    alpha = NA, stroke = 0.5),
   required_aes = c("x", "y", "p.value"),
 
-  draw_panel = function(self, data, panel_params, coord, conf.level = 0.05, r0 = 0.6) {
+  draw_panel = function(self, data, panel_params, coord, conf.level = 0.05) {
     data <- dplyr::filter(data, p.value >= conf.level)
-    dd <- point_to_cross(data$x, data$y, r0)
-    aesthetics <- setdiff(names(data), c("x", "y", "p.value"))
-    GeomSegment$draw_panel(cbind(dd, data[rep(1:nrow(data), each = 2), aesthetics]),
-                           panel_params, coord)
+    data$p.value <- NULL
+    GeomPoint$draw_panel(data, panel_params, coord)
   },
-  draw_key = draw_key_blank
+  draw_key = draw_key_point
 )
-
-#' @noRd
-point_to_cross <- function(x, y, r0 = 0.6) {
-  xx <- c(x - 0.5 * r0, x - 0.5 * r0)
-  xend <- c(x + 0.5 * r0, x + 0.5 * r0)
-  yy <- c(y - 0.5 * r0, y + 0.5 * r0)
-  yend <- c(y + 0.5 * r0, y - 0.5 * r0)
-
-  new_data_frame(list(
-    x = xx,
-    y = yy,
-    xend = xend,
-    yend = yend
-  ))
-}
-
--- R/scale-radius.R
+++ R/scale-radius.R
@@ -1,41 +1,32 @@
 #' @export
 scale_abs_area_radius <- function(name = waiver(), breaks = waiver(), labels = waiver(),
-                                  limits = NULL, range = c(-1, 1), mid = 0,
+                                  limits = NULL, range = c(-1, 1), from = c(-1, 1), mid = 0,
                                   trans = "identity", guide = NULL) {
-  continuous_scale("r0", "abs_area_radius", abs_area_pal(range), name = name, rescaler = mid_rescale2(mid),
+  continuous_scale("r0", "abs_area_radius", abs_area_pal(range), name = name, rescaler = mid_rescale2(mid, from),
                    breaks = breaks, labels = labels, limits = limits, trans = trans,
                    guide = guide)
 }
 
 scale_area_radius <- function(name = waiver(), breaks = waiver(), labels = waiver(),
-                                  limits = NULL, max = 1, mid = 0,
+                                  limits = NULL, max = 1, from = c(-1, 1), mid = 0,
                                   trans = "identity", guide = NULL) {
-  continuous_scale("r0", "area_radius", area_pal(max), name = name, rescaler = mid_rescale2(mid),
+  continuous_scale("r0", "area_radius", area_pal(max), name = name, rescaler = mid_rescale2(mid, from),
                    breaks = breaks, labels = labels, limits = limits, trans = trans,
                    guide = guide)
 }
 
 #' @export
 scale_rho <- function(name = waiver(), breaks = waiver(), labels = waiver(),
-                      limits = NULL, range = c(-1, 1), mid = 0,
+                      limits = NULL, range = c(-1, 1), from = c(-1, 1), mid = 0,
                       trans = "identity", guide = NULL) {
-  continuous_scale("rho", "rho", rescale_pal2(range), 
-                   name = name, rescaler = mid_rescale2(mid),
+  continuous_scale("rho", "rho", rescale_pal2(range),
+                   name = name, rescaler = mid_rescale2(mid, from),
                    breaks = breaks, labels = labels, limits = limits, trans = trans,
                    guide = guide)
 }
 
-#' @export
-scale_confidence <- function(name = waiver(), breaks = waiver(), labels = waiver(),
-                      limits = NULL, max = 0.6, conf.level = 0.05, trans = "identity", 
-                      guide = NULL) {
-  di("p.value", "confidence", conf_pal(max = max, conf.level = conf.level), name = name, 
-                   breaks = breaks, labels = labels, limits = limits, trans = trans, 
-                   guide = guide)
-}
-
 #' @noRd
-abs_area_pal <- function (range = c(-1, 1)) 
+abs_area_pal <- function (range = c(-1, 1))
 {
   force(range)
   function(x) {
@@ -44,7 +35,7 @@ abs_area_pal <- function (range = c(-1, 1))
 }
 
 #' @noRd
-area_pal <- function (max = 1) 
+area_pal <- function (max = 1)
 {
   force(max)
   function(x) {
@@ -53,7 +44,7 @@ area_pal <- function (max = 1)
 }
 
 #' @noRd
-rescale_pal2 <- function (range = c(-1, 1)) 
+rescale_pal2 <- function (range = c(-1, 1))
 {
   force(range)
   function(x) {
@@ -62,18 +53,8 @@ rescale_pal2 <- function (range = c(-1, 1))
 }
 
 #' @noRd
-conf_pal <- function (max = 0.6, conf.level = 0.05) 
-{
-  force(max)
-  force(conf.level)
-  function(x) {
-    ifelse(x >= conf.level, max, 0)
-  }
-}
-
-#' @noRd
-mid_rescale2 <- function(mid) {
-  function(x, to = c(-1, 1), from = range(x, na.rm = TRUE)) {
+mid_rescale2 <- function(mid, from = c(-1, 1)) {
+  function(x, to = c(-1, 1)) {
     scales::rescale_mid(x, to, from, mid)
   }
 }
