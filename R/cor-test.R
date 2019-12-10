f58635f39d59132094a8fa49e67c0574015ff9b5
houyun <houyunhuang@163.com>
2019-12-10 21:35:32 +0800

    re-write correlation fun

--- R/cor-test.R
+++ R/cor-test.R
@@ -3,60 +3,69 @@
 #'     each pair of input features.
 #' @param x a matrix object.
 #' @param y a matrix object or NULL.
-#' @param alternative indicates the alternative hypothesis and must be one of "two.sided",
-#'     "greater" or "less".
+#' @cor.test logical, if \code{TRUE} (default) will test for correlation.
 #' @param method a character string indicating which correlation coefficient is to be used
 #'     for the test. One of "pearson", "kendall", or "spearman".
+#' @param use an optional character string giving a method for computing covariances in the presence of missing values.
 #' @param ... extra params passing to `cor.test()`.
 #' @details The columns of 'x' will be tested for each pair when y is NULL(the default),
 #'     otherwise each column in 'x' and each column in 'y' is tested for each pair.
-#' @return a list with P values matrix, upper of confidence intervals matrix and lower of
+#' @return a list with correlation matrix, P values matrix, upper of confidence intervals matrix and lower of
 #'     confidence intervals matrix.
 #' @importFrom stats cor.test
+#' @importFrom purrr walk2
 #' @examples
 #' cor_test(mtcars)
 #' m1 <- matrix(rnorm(100), nrow = 10)
 #' m2 <- matrix(rnorm(60), nrow = 10)
-#' cor_test(m1, m2)
+#' correlate(m1, m2)
 #' @seealso \code{\link[stats]{cor.test}}.
 #' @author Houyun Huang
 #' @author Lei Zhou
 #' @author Jian Chen
 #' @author Taiyun Wei
 #' @export
-cor_test <- function(x,
-                     y = NULL,
-                     alternative = "two.sided",
-                     method = "pearson",
-                     ...) {
-  x <- as.matrix(x)
-  if(!is.null(y)) {
+correlate <- function(x,
+                 y = NULL,
+                 cor.test = FALSE,
+                 method = "pearson",
+                 use = "everything",
+                 ...) {
+  y <- y %||% x
+  if(!is.matrix(x))
+    x <- as.matrix(x)
+  if(!is.matrix(y))
     y <- as.matrix(y)
-  } else {
-    y <- x
-  }
   n <- ncol(x)
   m <- ncol(y)
-  p <- low <- upp <- matrix(NA, ncol = m, nrow = n)
-  for (i in 1:n) {
-    for (j in 1:m) {
-      suppressWarnings(
-        tmp <- cor.test(x = x[ , i], y = y[ , j], alternative = alternative,
-                                       method = method, ...)
-        )
-      p[i, j] <- tmp$p.value
-
-      # only "pearson" method provides confidence intervals
-      if (method == "pearson") {
-        low[i, j] <- tmp$conf.int[1]
-        upp[i, j] <- tmp$conf.int[2]
+  r <- cor(x, y, use = use, method = method)
+  p.value <- lower.ci <- upper.ci <- matrix(NA, ncol = m, nrow = n)
+  if(cor.test) {
+    df <- expand.grid(1:n, 1:m)
+    purrr::walk2(df$Var1, df$Var2, function(.idx, .idy) {
+      tmp <- cor.test(x = x[ , .idx], y = y[ , .idy], method = method, ...)
+      p.value[.idx, .idy] <<- tmp$p.value
+      if(method == "pearson") {
+        if (nrow(x) > 3) {
+          lower.ci[.idx, .idy] <<- tmp$conf.int[1]
+          upper.ci[.idx, .idy] <<- tmp$conf.int[2]
+        } else {
+          warning("correlation test interval at least needs 4 observations.", call. = FALSE)
+        }
       }
-    }
+    })
   }
-
-  list(
-    p = p,
-    low = low,
-    upp = upp
+  structure(
+    .Data = list(
+      r = r,
+      p.value = p.value,
+      lower.ci = lower.ci,
+      upper.ci = upper.ci
+    ), class = "correlation"
   )
+}
+
+#' @noRd
+print.correlation <- function(x, all = FALSE, ...) {
+  if(all) print(x, ...) else print(x$r, ...)
 }
