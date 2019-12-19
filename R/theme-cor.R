d0636eee20d44e52dbd7bc2fbb021bb87624abc7
houyun <houyunhuang@163.com>
2019-12-19 21:03:57 +0800

    adjust axis label size

--- R/theme-cor.R
+++ R/theme-cor.R
@@ -20,7 +20,7 @@ theme_cor <- function(legend.position = "right",
                       ...)
 {
   theme(
-    axis.text = element_text(size = 12, colour = "black"),
+    axis.text = element_text(size = 10.5, colour = "black"),
     axis.title = element_blank(),
     axis.line = element_blank(),
     axis.text.x.top = element_text(angle = 45, hjust = 0, vjust = 0),
@@ -38,13 +38,13 @@ theme_cor2 <- function(legend.position = "right",
                       ...)
 {
   theme(
-    axis.text = element_text(size = 12, colour = "black"),
+    axis.text = element_text(size = 10.5, colour = "black"),
     axis.title = element_blank(),
     axis.line = element_blank(),
     axis.ticks = element_blank(),
     axis.text.x.top = element_text(angle = 45, hjust = 0, vjust = 0),
     axis.text.x.bottom = element_text(angle = 45, hjust = 1, vjust = 1),
-    panel.background = element_rect(fill = NA),
+    panel.background = element_rect(fill = NA, colour = "grey50"),
     panel.grid = element_line(colour = "grey50", size = 0.25),
     legend.position = legend.position,
     ...
