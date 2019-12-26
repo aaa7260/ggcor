03aca0b9f0479d24f3a70a314800e32b6c8f3e42
houyun <houyunhuang@163.com>
2019-12-26 21:38:31 +0800

    delete unuseful cor_tbl helper function

--- R/cor-tbl-utils.R
+++ R/cor-tbl-utils.R
@@ -1,9 +1,7 @@
-#' Get attributes of cor_tbl
-#' @description Helper function to get extra attributs of cor_tbl.
+#' Helper function of cor_tbl
 #' @param x a cor_tbl.
-#' @return return attribute vector.
-#' @rdname cor-tbl-attr
-#' @export
+#' @return return attribute value.
+#' @rdname cor_tbl_attr
 #' @examples
 #' df <- fortify_cor(mtcars)
 #' ## get xname
@@ -14,71 +12,37 @@
 #' cor_tbl_showdiag(df)
 #' ## get type
 #' cor_tbl_type(df)
-#' @author Houyun Huang
-#' @author Lei Zhou
-#' @author Jian Chen
-#' @author Taiyun Wei
+#' @author Houyun Huang, Lei Zhou, Jian Chen, Taiyun Wei
 #' @export
 cor_tbl_xname <- function(x) {
   stopifnot(is_cor_tbl(x) || is_cor_tbl_fct(x))
   attr(x, "xname")
 }
-#' @rdname cor-tbl-attr
+#' @rdname cor_tbl_attr
 #' @export
 cor_tbl_yname <- function(x) {
   stopifnot(is_cor_tbl(x) || is_cor_tbl_fct(x))
   attr(x, "yname")
 }
-#' @rdname cor-tbl-attr
+#' @rdname cor_tbl_attr
 #' @export
 cor_tbl_type <- function(x) {
   stopifnot(is_cor_tbl(x))
   attr(x, "type")
 }
-#' @rdname cor-tbl-attr
+#' @rdname cor_tbl_attr
 #' @export
 cor_tbl_showdiag <- function(x) {
   stopifnot(is_cor_tbl(x) || is_cor_tbl_fct(x))
   attr(x, "show.diag")
 }
-#' @rdname cor-tbl-attr
-#' @export
+#' @noRd
 is_cor_tbl <- function(x) {
   inherits(x, "cor_tbl")
 }
 
-#' Coerce cor_tbl to tbl_df
-#' @param x cor_tbl object
-#' @param rm.extra.attr a logical value indicating whether remove the extra attributes.
-#' @return a tbl_df object.
-#' @examples
-#' df <- fortify_cor(mtcars)
-#' cor_tbl2tbl_df(df)
-#' @export
-cor_tbl2tbl_df <- function(x, rm.extra.attr = TRUE) {
-  stopifnot(is_cor_tbl(x))
-  class(x) <- setdiff(class(x), "cor_tbl")
-  if(rm.extra.attr) {
-    attr(x, c("xname", "yname", "type", "show.diag")) <- NULL
-  }
-  x
-}
-#' Print a cor_tbl object
-#' @param x A cor_tbl object.
-#' @param n The max lines will be print (the default is 5).
-#' @param width The max width for each line.
-#' @param ... Extra params passing to `print()` function.
-#' @export
-#' @method print cor_tbl
-#' @author Houyun Huang
-print.cor_tbl <- function(x, n = 5, width = 60, ...) {
-  tibble:::print.tbl(x, n = n, width = width, ...)
-  cat("Extra attributes:\n")
-  cat_line(cor_tbl_xname(x), name = "xname:", width = width)
-  cat_line(cor_tbl_yname(x), name = "yname:", width = width)
-  cat_line(cor_tbl_type(x), name = "type:", width = width)
-  cat_line(cor_tbl_showdiag(x), name = "show.diag:", width = width)
-}
+
+
 
 
 
