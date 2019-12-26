fea42716bc32666d253b4d50addfb7b7dbb9c67a
houyun <houyunhuang@163.com>
2019-12-26 22:33:05 +0800

    update get_data help doc

--- R/get-data.R
+++ R/get-data.R
@@ -1,3 +1,24 @@
+#' Helper function to extract cor_tbl.
+#' @description These functions are used to quickly obtain the upper
+#'     trig, lower trig, diagonal, or remove the diagonal of the correlation
+#'     coefficient matrix.
+#' @param x a cor_tbl object.
+#' @param show.diag a logical value indicating whether keep the diagonal.
+#' @return a modified cor_tbl object.
+#' @importFrom dplyr filter
+#' @rdname extract_cor_tbl
+#' @examples
+#' df <- fortify_cor(mtcars)
+#' quickcor(df) + geom_colour()
+#' df01 <- get_lower_data(df)
+#' quickcor(df01) + geom_colour()
+#' get_upper_data(df, show.diag = FALSE)
+#' quickcor(df02) + geom_colour()
+#' df03 <- get_diag_data(df)
+#' quickcor(df03) + geom_colour()
+#' df04 <- get_diag_tri(df)
+#' quickcor(df04) + geom_colour()
+#' @author Houyun Huang, Lei Zhou, Jian Chen, Taiyun Wei
 #' @export
 get_lower_data <- function(x, show.diag = TRUE)
 {
@@ -16,6 +37,7 @@ get_lower_data <- function(x, show.diag = TRUE)
   attr(x, "show.diag") <- show.diag
   x
 }
+#' @rdname extract_cor_tbl
 #' @export
 get_upper_data <- function(x, show.diag = TRUE)
 {
@@ -34,7 +56,7 @@ get_upper_data <- function(x, show.diag = TRUE)
   attr(x, "show.diag") <- show.diag
   x
 }
-
+#' @rdname extract_cor_tbl
 #' @export
 get_diag_tri <- function(x)
 {
@@ -49,7 +71,7 @@ get_diag_tri <- function(x)
     attr(x, "show.diag") <- FALSE
   x
 }
-
+#' @rdname extract_cor_tbl
 #' @export
 get_diag_data <- function(x)
 {
@@ -62,6 +84,25 @@ get_diag_data <- function(x)
   dplyr::filter(x, xx + yy == n + 1)
 }
 
+#' Create cor_tbl extractor function
+#' @description This function returns another function that can extract cor_tbl
+#'     subset from a cor_tbl object.
+#' @param type a string, "full" (default), "upper" or "lower", display full,
+#'     lower triangular or upper triangular matrix.
+#' @param show.diag a logical value indicating whether keep the diagonal.
+#' @param ... extra filter params, see Details.
+#' @details This function is mainly used in \code{ggplot2} geom_*() functions,
+#'     where data is filtered based on the \code{...} parameter, then subsets
+#'     are extracted based on the type and show.diag parameters.
+#' @return extractor function
+#' @importFrom dplyr filter
+#' @rdname get_data
+#' @examples
+#' quickcor(mtcars) +
+#'   geom_square(data = get_data(type = "upper")) +
+#'   geom_circle2(data = get_data(type = "lower", show.diag = FALSE))
+#' @seealso \code{\link[dplyr]{filter}}.
+#' @author Houyun Huang, Lei Zhou, Jian Chen, Taiyun Wei
 #' @export
 get_data <- function(..., type = "full", show.diag = TRUE)
 {
