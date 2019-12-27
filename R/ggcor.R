e2ba437f275909d7b4169497ce9efc4beaef4860
houyun <houyunhuang@163.com>
2019-12-27 20:52:20 +0800

    update readme

--- R/ggcor.R
+++ R/ggcor.R
@@ -1,10 +1,30 @@
+#' Create a correlation plot
+#' @description This function is the equivalent of \code{\link[ggplot2]{ggplot}}
+#'     in ggplot2. It takes care of setting up the position of axis and legend for
+#'     the plot based on the plot type.
+#' @param data cor_tbl object.
+#' @param mapping NULL (default) or a list of aesthetic mappings to use for plot,
+#'     see Details.
+#' @param axis.x.position,axis.y.position the position of the axis. 'auto' (default)
+#'     is set according to the plot type, 'bottom' or 'top' for x axes, 'left' or 'right'
+#'     for y axes.
+#' @param axis.label.drop logical value (default is TRUE). When type of plot is 'upper'
+#'     or 'lower' and 'show.diag' is FALSE, do you need to remove the blank coordinate
+#'     label.
+#' @return an object of class gg onto which layers, scales, etc. can be added.
+#' @rdname ggcor
+#' @examples
+#' df <- fortify_cor(mtcars)
+#' ggcor(df)
+#' df01 <- fortify_cor(mtcars, type = "lower", show.diag = FALSE)
+#' ggcor(df01, axis.label.drop = TRUE)
+#' @author Houyun Huang, Lei Zhou, Jian Chen, Taiyun Wei
 #' @export
 ggcor <- function(data,
                   mapping = NULL,
                   axis.x.position = "auto",
                   axis.y.position = "auto",
-                  axis.label.drop = TRUE,
-                  ...)
+                  axis.label.drop = TRUE)
 {
   if(!is_cor_tbl(data))
     stop("'data' needs a cor_tbl.", call. = FALSE)
@@ -49,8 +69,7 @@ ggcor <- function(data,
       }
     }
   }
-  envir <- parent.frame()
-  p <- ggplot(data = data, mapping = mapping, environment = envir) +
+  p <- ggplot(data = data, mapping = mapping, environment = parent.frame()) +
     scale_x_continuous(breaks = axis.x.breaks, labels = axis.x.labels,
                        position = axis.x.position)+
     scale_y_continuous(breaks = axis.y.breaks, labels = axis.y.labels,
