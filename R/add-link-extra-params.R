3a684a0a260e8e8c3f349cfe361a160a59023ad2
houyun <houyunhuang@163.com>
2019-12-28 23:40:53 +0800

    add extra_params help doc

--- R/add-link-extra-params.R
+++ R/add-link-extra-params.R
@@ -1,3 +1,14 @@
+#' Extra params for add_link
+#' @description This function is similar to the \code{theme} function in ggplot2,
+#'     used to set the shape, size, colour, fill of spec and env marker points,
+#'     size, colour, family, and so on of spec labels.
+#' @param spec.label text_params object (\code{\link[ggcor]{text_params}}).
+#' @param spec.point,env.point point_params object (\code{\link[ggcor]{point_params}}).
+#' @param link.params link_params object (\code{\link[ggcor]{link_params}}).
+#' @rdname extra_params
+#' @seealso \code{\link[ggcor]{text_params}}, \code{\link[ggcor]{point_params}},
+#'     \code{\link[ggcor]{link_params}}.
+#' @author Houyun Huang, Lei Zhou, Jian Chen, Taiyun Wei
 #' @export
 extra_params <- function(spec.label = text_params(),
                          spec.point = point_params(fill = "blue"),
@@ -19,7 +30,24 @@ extra_params <- function(spec.label = text_params(),
   ),
   class = "extra_params")
 }
-
+#' Extra params elements
+#' @description In conjunction with the \code{\link[ggcor]{extra_params}}.
+#' @param fill Fill colour.
+#' @param colour,color Line/border colour. Color is an alias for colour.
+#' @param size Line/border size in mm; text size in pts.
+#' @param alpha alpha level in [0,1].
+#' @param angle angle (in [0, 360]).
+#' @param hjust horizontal justification (in [0, 1]).
+#' @param vjust vertical justification (in [0, 1]).
+#' @param family font family.
+#' @param fontface font face.
+#' @param shape point shape.
+#' @param stroke point stroke.
+#' @param spec.point.hjust,env.point.hjust numeric vector, adjust horizontal position of points.
+#' @param spec.point.vjust,env.point.vjust numeric vector, adjust vertical position of points.
+#' @rdname extra_params_elements
+#' @seealso \code{\link[ggcor]{extra_params}}.
+#' @author Houyun Huang, Lei Zhou, Jian Chen, Taiyun Wei
 #' @export
 text_params <- function(colour = "black", size = 3.88, angle = 0, hjust = NULL,
                         vjust = 0.5, alpha = NA, family = "", fontface = 1, color = NULL)
@@ -29,7 +57,7 @@ text_params <- function(colour = "black", size = 3.88, angle = 0, hjust = NULL,
                  vjust = vjust, alpha = alpha, family = family, fontface = fontface),
             class = "text_params")
 }
-
+#' @rdname extra_params_elements
 #' @export
 point_params <- function(alpha = NA, colour = "black", fill = NA, shape = 21,
                          size = 1, stroke = 0.5, color = NULL)
@@ -39,7 +67,7 @@ point_params <- function(alpha = NA, colour = "black", fill = NA, shape = 21,
                  shape = shape, size = size, stroke = stroke),
             class = "point_params")
 }
-
+#' @rdname extra_params_elements
 #' @export
 link_params <- function(env.point.hjust = NULL, env.point.vjust = NULL,
                         spec.point.hjust = NULL, spec.point.vjust = NULL)
