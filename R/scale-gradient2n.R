3d9877ffbb10c7873316d36a30edc521e5705691
houyun <houyunhuang@163.com>
2020-01-01 14:02:14 +0800

    tidy scale-function

--- R/scale-gradient2n.R
+++ R/scale-gradient2n.R
@@ -3,15 +3,8 @@
 #' `scale_*_gradient2n` creates a n colour gradient (low-high), mixed some
 #' features of `scale_*_gradient2` and `scale_*_gradientn`.
 #'
-#' Default colours are `c("#67001F", "#B2182B", "#D6604D", "#F4A582","#FDDBC7",
-#' "#FFFFFF", "#D1E5F0", "#92C5DE","#4393C3", "#2166AC", "#053061")` , borrowed
-#' from \pkg{corrplot}.
-#'
 #' @inheritParams scales::seq_gradient_pal
-#' @inheritParams ggplot2::scale_colour_hue
-#' @param guide Type of legend. Use `"colourbar"` for continuous
-#'   colour bar, or `"legend"` for discrete colour legend.
-#' @inheritDotParams ggplot2::continuous_scale -na.value -guide -aesthetics
+#' @inheritParams ggplot2::scale_fill_gradient
 #' @seealso [scales::seq_gradient_pal()] for details on underlying
 #'   palette
 #' @importFrom ggplot2 continuous_scale
@@ -79,49 +72,6 @@ scale_fill_gradient2n <- function(...,
                    rescaler = mid_rescaler(mid = midpoint))
 }
 
-#' #' @rdname scale_colour_gradient2n
-#' #' @export
-#' scale_fill_steps2n <- function(...,
-#'                                colours,
-#'                                midpoint = 0,
-#'                                values = NULL,
-#'                                space = "Lab",
-#'                                limits = c(-1, 1),
-#'                                na.value = "grey50",
-#'                                guide = "coloursteps",
-#'                                aesthetics = "fill",
-#'                                colors = NULL)
-#' {
-#'   colours <- if (missing(colours)) colors else colours
-#'   colours <- colours %||% red_blue()
-#'   ggplot2:::binned_scale(aesthetics, "steps2n", scales::gradient_n_pal(colours, values, space),
-#'                          limits = limits, na.value = na.value, guide = guide,
-#'                          rescaler = mid_rescaler(mid = midpoint), ...)
-#' }
-#'
-#' #' @rdname scale_colour_gradient2n
-#' #' @export
-#' scale_colour_steps2n <- function(...,
-#'                                  colours,
-#'                                  midpoint = 0,
-#'                                  values = NULL,
-#'                                  space = "Lab",
-#'                                  limits = c(-1, 1),
-#'                                  na.value = "grey50",
-#'                                  guide = "coloursteps",
-#'                                  aesthetics = "colour",
-#'                                  colors = NULL)
-#' {
-#'   colours <- if (missing(colours)) colors else colours
-#'   colours <- colours %||% red_blue()
-#'   ggplot2:::binned_scale(aesthetics, "steps2", scales::gradient_n_pal(colours, values, space),
-#'                          limits = limits, na.value = na.value, guide = guide,
-#'                          rescaler = mid_rescaler(mid = midpoint), ...)
-#' }
-#'
-#' #' @rdname scale_colour_gradient2n
-#' #' @export
-#' scale_color_steps2n <- scale_colour_steps2n
 #' @noRd
 mid_rescaler <- function(mid) {
   function(x, to = c(0, 1), from = range(x, na.rm = TRUE)) {
