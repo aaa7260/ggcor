c4928f529dd241be53be0b20045ea4151dde38fd
houyun <houyunhuang@163.com>
2020-01-14 21:04:18 +0800

    fast create network

--- R/tidy-network-data.R
+++ R/tidy-network-data.R
@@ -7,26 +7,47 @@
 #' @param p.thres a numeric value.
 #' @param ... passing to \code{\link[ggcor]{as_cor_tbl}}
 #' @return cor_tbl.
-#' @rdname tidy_network_data
+#' @rdname create_network
 #' @export
-tidy_network_data <- function(x,
-                              r.thres = 0.6,
-                              r.absolute = TRUE,
-                              p.thres = NULL,
-                              type = "upper",
-                              show.diag = FALSE,
-                              cor.test = TRUE,
-                              ...)
+create_network <- function(x,
+                           r.thres = 0.6,
+                           r.absolute = TRUE,
+                           p.thres = 0.05,
+                           ...)
 {
-  df <- fortify_cor(x, type = type, show.diag = show.diag,
-                    cor.test = cor.test, ...)
+  df <- fortify_cor(x, type = "upper", show.diag = FALSE, cor.test = TRUE, ...)
   if(r.absolute) {
-    df <- with(df, subset(df, abs(r) > r.thres))
+    df <- with(df, subset(df, abs(r) > r.thres, p.value < p.thres))
   } else {
-    df <- with(df, subset(df, r > r.thres))
-  }
-  if(!is.null(p.thres)) {
-    df <- with(df, subset(df, p.value < p.thres))
+    df <- with(df, subset(df, r > r.thres, p.value < p.thres))
   }
   df
+}
+
+#' @importFrom tibble tibble
+#' @rdname create_network
+#' @export
+fast_create_network <- function(x,
+                                r.thres = 0.6,
+                                r.absolute = TRUE,
+                                p.thres = 0.05,
+                                ...)
+{
+  if(!requireNamespace("WGCNA", quietly = TRUE)) {
+    stop("'fast_correlate' needs 'WGCNA' package.", call. = FALSE)
+  }
+  if(!is.data.frame(x))
+    x <- as.data.frame(x)
+  name <- names(x)
+  corr <- WGCNA::corAndPvalue(x, ...)
+  idx <- upper.tri(corr$cor)
+  df <- tibble::tibble(.row.names = rep(name, length(name)),
+                       .col.names = rep(name, each = length(name)),
+                       r = as.vector(corr$cor),
+                       p.value = as.vector(corr$p)) %>% subset(idx)
+  if(r.absolute) {
+    with(df, subset(df, abs(r) > r.thres, p.value < p.thres))
+  } else {
+    with(df, subset(df, r > r.thres, p.value < p.thres))
+  }
 }
