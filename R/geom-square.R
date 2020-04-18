6120b9cbb9934e57add4ec75dac82ed54d000569
houyun <houyunhuang@163.com>
2020-04-18 13:12:25 +0800

    use geom_rect to draw square

--- R/geom-square.R
+++ R/geom-square.R
@@ -1,7 +1,7 @@
 #' Square Geom
 #'
 #' @inheritParams ggplot2::layer
-#' @inheritParams ggplot2::geom_polygon
+#' @inheritParams ggplot2::geom_rect
 #' @section Aesthetics:
 #' \code{geom_square()}, \code{geom_upper_square()} and \code{geom_lower_square()}
 #' understands the following aesthetics (required aesthetics are in bold):
@@ -27,17 +27,19 @@
 #'       \item \code{lower_linetype}
 #'       \item \code{lower_size}
 #'    }
-#' @importFrom ggplot2 layer ggproto GeomPolygon aes
+#' @importFrom ggplot2 layer ggproto GeomRect aes
 #' @importFrom grid grobTree
 #' @rdname geom_square
 #' @author Houyun Huang, Lei Zhou, Jian Chen, Taiyun Wei
 #' @export
-geom_square <- function(mapping = NULL, data = NULL,
-                         stat = "identity", position = "identity",
-                         ...,
-                         na.rm = FALSE,
-                         show.legend = NA,
-                         inherit.aes = TRUE) {
+geom_square <- function(mapping = NULL,
+                        data = NULL,
+                        stat = "identity",
+                        position = "identity",
+                        ...,
+                        na.rm = FALSE,
+                        show.legend = NA,
+                        inherit.aes = TRUE) {
   layer(
     data = data,
     mapping = mapping,
@@ -108,15 +110,15 @@ geom_lower_square <- function(mapping = NULL, data = get_data(type = "lower"),
 #' @usage NULL
 #' @export
 GeomSquare <- ggproto(
-  "GeomSquare", GeomPolygon,
+  "GeomSquare", GeomRect,
   default_aes = aes(r0 = 0.5, colour = "grey35", fill = NA, size = 0.25, linetype = 1,
                     alpha = NA),
   required_aes = c("x", "y"),
   draw_panel = function(self, data, panel_params, coord, linejoin = "mitre") {
-    aesthetics <- setdiff(names(data), c("x", "y", "group", "subgroup"))
+    aesthetics <- setdiff(names(data), c("x", "y", "xmin", "ymin", "xmax", "ymax"))
     dd <- point_to_square(data$x, data$y, data$r0)
-    aes <- data[rep(1:nrow(data), 5) , aesthetics, drop = FALSE]
-    GeomPolygon$draw_panel(cbind(dd, aes), panel_params, coord)
+    data <- cbind(dd, data[, aesthetics, drop = FALSE])
+    GeomPolygon$draw_panel(data, panel_params, coord)
   },
 
   draw_key = draw_key_square
@@ -162,14 +164,10 @@ GeomLowerSquare <- ggproto(
 point_to_square <- function(x, y, r0) {
   r0 <- 0.5 * sign(r0) * sqrt(abs(r0))
   n <- length(x)
-  xmin <- - r0 + x
-  xmax <- r0 + x
-  ymin <- - r0 + y
-  ymax <- r0 + y
   new_data_frame(list(
-    x = c(xmin, xmax, xmax, xmin, xmin),
-    y = c(ymax, ymax, ymin, ymin, ymax),
-    group = rep(1:n, 5)
-  ))
+    xmin = - r0 + x,
+    xmax = r0 + x,
+    ymin = - r0 + y,
+    ymax = r0 + y))
 }
 
