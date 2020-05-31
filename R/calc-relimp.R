741c7fa987230b9e47f077b07a190a4b4cee43c2
houyun <houyunhuang@163.com>
2020-05-31 17:26:46 +0800

    calc_relimp

---
+++ R/calc-relimp.R
@@ -0,0 +1,69 @@
+#' Relative importance
+#' @title Relative importance
+#' @param spec,env a data.frame object.
+#' @param byrow a logical value, if TRUE, the 'spec' on the rows.
+#' @param seed a integer value.
+#' @param x a calc_relimp object.
+#' @param ... extra parameters.
+#' @return a calc_relimp object.
+#' @rdname calc_relimp
+#' @examples \dontrun{
+#' spec <- mtcars[c(1, 3, 4, 5)]
+#' env <- mtcars[6:11]
+#' calc_relimp(spec, env)
+#' }
+#' @author Houyun Huang, Lei Zhou, Jian Chen, Taiyun Wei
+#' @export
+calc_relimp <- function(spec,
+                        env,
+                        type = "lmg",
+                        byrow = FALSE,
+                        ...)
+{
+  type <- match.arg(type, c("lmg", "last", "first", "betasq", "pratt", "genizi", "car"))
+  if(!is.data.frame(spec))
+    spec <- as.data.frame(spec)
+  if(!is.data.frame(env))
+    env <- as.data.frame(env)
+
+  n <- length(spec)
+  m <- length(env)
+  if(any(n < 1, m < 1)) {
+    stop("Zero length data.", call. = FALSE)
+  }
+  calc.relimp <- get_function("relaimpo", "calc.relimp")
+
+  explained <- structure(.Data = vector(length = n), names = names(spec), class = "numeric")
+  if(byrow) {
+    importance <- matrix(NA, nrow = n, ncol = m, dimnames = list(names(spec), names(env)))
+  } else {
+    importance <- matrix(NA, nrow = m, ncol = n, dimnames = list(names(env), names(spec)))
+  }
+  for (i in seq_len(n)) {
+    cr <- calc.relimp(spec[[i]] ~ ., data = env, importance = TRUE, ...)
+    explained[i] <- extrac_s4(cr, "R2") * 100
+    if(isTRUE(byrow)) {
+      importance[i, ] <- extrac_s4(cr, type)
+    } else {
+      importance[, i] <- extrac_s4(cr, type)
+    }
+  }
+  structure(.Data = list(explained = as.data.frame(explained),
+                         importance = as.data.frame(importance)),
+            class = "calc_relimp")
+}
+
+#' @rdname calc_relimp
+#' @export
+print.calc_relimp <- function(x, ...) {
+  cat("Var explained (%):\n")
+  print(x$explained)
+  cat("\n")
+  cat("Var importance:\n")
+  print(x$importance)
+}
+
+#' @noRd
+extrac_s4 <- function(x, e) {
+  do.call("@", list(x, e))
+}
