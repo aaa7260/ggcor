a9e0e7407dafc3ba8d43c96745a2750308e16ec9
houyun <houyunhuang@163.com>
2020-06-25 16:32:23 +0800

    add start message

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,12 @@
-.onAttach  <- function(libname, pkgname )
+.onAttach  <- function(libname, pkgname)
 {
-  options(
-    ggcor.fill.pal = c("#67001F", "#B2182B", "#D6604D", "#F4A582",
-                       "#FDDBC7", "#F7F7F7", "#D1E5F0", "#92C5DE",
-                       "#4393C3", "#2166AC", "#053061"),
-    ggcor.plot.style = "corrplot")
+  packageStartupMessage("\n********************************************************")
+  packageStartupMessage("Note: As of version 0.9.8, ggcor does not change the")
+  packageStartupMessage("  default ggplot2 continuous fill scale anymore. To")
+  packageStartupMessage("  recover the previous behavior, execute:")
+  packageStartupMessage("  set_scale()")
+  packageStartupMessage("  Instead of using the set_scale() function, we")
+  packageStartupMessage("  recommend adding the 'scale_fill_*()' function")
+  packageStartupMessage("  to the plot as needed.")
+  packageStartupMessage("********************************************************\n")
 }
